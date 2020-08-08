facefb48e1d7b0d182837b8d76a83249a120138e
slam74 <63279467+slam74@users.noreply.github.com>
2020-08-08 14:09:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 // category selection menu bar
+
 // product contents to be displayed on the page
-var allHeader = document.querySelector('#shop-all');
-var racquets = document.querySelector('.racquets');
-var shoes = document.querySelector('.shoes');
-var shuttlecocks = document.querySelector('.shuttlecocks');
+ var allHeader = document.querySelector('#shop-all');
+ var racquets = document.querySelector('.racquets');
+ var shoes = document.querySelector('.shoes');
+ var shuttlecocks = document.querySelector('.shuttlecocks');
  
 // function clears the main element of all categories
 function clear() {
-  var content = document.querySelector('main');
-  while (content.firstChild) {
-    content.removeChild(content.firstChild);
-  }
-}
+   var content = document.querySelector('main');
+   while (content.firstChild) {
+     content.removeChild(content.firstChild);
+   }
+ }
  
 // function for filtering display based on the category clicked on the menu bar
-function menuSelection(choice, display) {
-  document.querySelector(choice).addEventListener('click', function() {
-    clear();
-    document.querySelector('main').appendChild(display);
-  });
-}
+ function menuSelection(choice, display) {
+   document.querySelector(choice).addEventListener('click', function() {
+     clear();
+     document.querySelector('main').appendChild(display);
+   });
+ }
  
 // if racquets is clicked, only display racquet products
-menuSelection('#racquets-category', racquets);
+ menuSelection('#racquets-category', racquets);
 
 // if shoes is clicked, only display shoes products
-menuSelection('#shoes-category', shoes);
+ menuSelection('#shoes-category', shoes);
 
 // if shuttlecocks is clicked, display only shuttlecocks
-menuSelection('#shuttlecocks-category', shuttlecocks);
+ menuSelection('#shuttlecocks-category', shuttlecocks);
 
 // if shop all is clicked, display all item categories
-document.querySelector('#all-categories').addEventListener('click', function() {
-  clear();
-  document.querySelector('main').appendChild(allHeader);
-  document.querySelector('main').appendChild(racquets);
-  document.querySelector('main').appendChild(shoes);
-  document.querySelector('main').appendChild(shuttlecocks);
+ document.querySelector('#all-categories').addEventListener('click', function() {
+   clear();
+   document.querySelector('main').appendChild(allHeader);
+   document.querySelector('main').appendChild(racquets);
+   document.querySelector('main').appendChild(shoes);
+   document.querySelector('main').appendChild(shuttlecocks);
 });
