c6528fdbe757a5e0590eb7d7d99a80f77a9d9d00
slam74 <63279467+slam74@users.noreply.github.com>
2020-08-08 22:43:05 -0400

    Update contact-script.js

--- contact-script.js
+++ contact-script.js
@@ -1,17 +1,22 @@
 // contact-email-form radio buttons and order number input display
 window.addEventListener('load', function(){
     document.querySelector('#order-number').style.display = 'none';
+    document.querySelector('#order-number-label').style.display = 'none';
   });
   
   document.querySelector('#order-problem-option').addEventListener('click', function(){
     document.querySelector('#order-number').style.visibility = 'visible';
     document.querySelector('#order-number').style.display = 'block';
+    document.querySelector('#order-number-label').style.visibility = 'visible';
+    document.querySelector('#order-number-label').style.display = 'block';
   });
   
   document.querySelector('#comment-option').addEventListener('click', function(){
     document.querySelector('#order-number').style.display = 'none';
+    document.querySelector('#order-number-label').style.display = 'none';
   });
   
   document.querySelector('#question-option').addEventListener('click', function(){
     document.querySelector('#order-number').style.display = 'none';
+    document.querySelector('#order-number-label').style.display = 'none';
   });
