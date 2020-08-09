a66d4741d35201b2fa7e2a4975c98348c3084a2f
slam74 <63279467+slam74@users.noreply.github.com>
2020-08-08 20:31:16 -0400

    Add files via upload

---
+++ contact-script.js
@@ -0,0 +1,17 @@
+// contact-email-form radio buttons and order number input display
+window.addEventListener('load', function(){
+    document.querySelector('#order-number').style.display = 'none';
+  });
+  
+  document.querySelector('#order-problem-option').addEventListener('click', function(){
+    document.querySelector('#order-number').style.visibility = 'visible';
+    document.querySelector('#order-number').style.display = 'block';
+  });
+  
+  document.querySelector('#comment-option').addEventListener('click', function(){
+    document.querySelector('#order-number').style.display = 'none';
+  });
+  
+  document.querySelector('#question-option').addEventListener('click', function(){
+    document.querySelector('#order-number').style.display = 'none';
+  });
